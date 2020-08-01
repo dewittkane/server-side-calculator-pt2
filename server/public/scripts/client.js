6db99cf84aa17c7ce7650325bc4b087dd4396872
DeWitt Kane <dewitt.kane@gmail.com>
2020-08-01 10:09:06 -0500

    post calc function added, collecting inputs. calculate function working on server side

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,11 +1,40 @@
 $(document).ready(function(){
     console.log('js and jq loaded!');
     
+    $('#equalsBtn').on('click', postCalc);
+    //click listeners
 
 
 
+    function postCalc(){
+        let newCalculation = {
+            firstNum: $('#numOneIn').val(),
+            operator: $('#operators').val(),
+            secondNum: $('#numTwoIn').val()
+        };
+        console.log(newCalculation);
+        if (newCalculation.firstNum !== '' && newCalculation.secondNum !== '') {
+            if (newCalculation.operator === '/' && newCalculation.secondNum === '0'){
+                alert("You can't divide by zero! Please enter a valid second number.");
+                return false;     
+            }; //This will catch trying to divide by zero
+        
+            $.ajax({
+                method: 'post',
+                url: '/calculations',
+                data: newCalculation
+            }).then(function(response){
+                console.log('coming back from post');
+                getLastCalc(response);
+                //getPreviousCalcs()
+            });//ajax sends new calculation to server and comes back with most recent calculation
 
-
+        } else {
+            alert('Please enter both number fields');
+            return false;
+        };//this will catch having an empty input
+        
+    };
 
 
 
--- server/server.js
+++ server/server.js
@@ -8,3 +8,30 @@ app.listen(port, () => {
     console.log('Listening on port:', port); 
 });
 //server boilerplate
+
+let previousCalculation = [];
+
+app.post('/calculations', (req, res) => {
+    let calculationIn = req.body;
+    console.log('in server.js, pre calculation', calculationIn);
+    let calculationOut = calculate(calculationIn);
+    console.log('in server.js, post calculation', calculationOut);
+    res.send(calculationOut);
+    
+})
+
+function calculate(calculationObject){
+    if (calculationObject.operator === '+') {
+        calculationObject.result = parseFloat(calculationObject.firstNum) + parseFloat(calculationObject.secondNum)
+        return calculationObject;
+    } else if (calculationObject.operator === '-') {
+        calculationObject.result = parseFloat(calculationObject.firstNum) - parseFloat(calculationObject.secondNum)
+        return calculationObject;
+    } else if (calculationObject.operator === '*') {
+        calculationObject.result = parseFloat(calculationObject.firstNum) * parseFloat(calculationObject.secondNum)
+        return calculationObject;
+    } else if (calculationObject.operator === '/') {
+        calculationObject.result = parseFloat(calculationObject.firstNum) / parseFloat(calculationObject.secondNum)
+        return calculationObject;
+    }
+}
